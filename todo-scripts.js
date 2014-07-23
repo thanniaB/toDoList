cdb306033b9e347d69cd622ded3bf3a465e59966
Thannia Blanchet <thannia.blanchet@gmail.com>
2014-07-23 12:59:25 -0700

    finished first version

--- todo-scripts.js
+++ todo-scripts.js
@@ -10,6 +10,7 @@
 
 		function check(){			
 			//if not checked add +1 items on the list
+			checkboxes = $('main :checkbox');
 			totalItems = checkboxes.length;
 			checkboxes.each(
 				function(){
@@ -25,6 +26,9 @@
 				} else {
 					$(".info").text(totalItems + " items left");
 				}
+				//dont forget to delete checked ones
+			} else {
+				$(".info").css("visibility", "hidden");
 			}
 			
 		}  
@@ -34,11 +38,28 @@
     		if(e.which == 13) {//tecla enter
     			//anadir tarea  
     			textBox.after("<div class='task'><input type='checkbox' class='checkbox' /> "+ inputText + "<input type='submit' class='eraseButton' value='X' /></div>");    			
-				checkboxes = $('main :checkbox');				
-				tasks = $(".task");				
+						
     			check();		
     		}
 		});
+
+		$("main").on("click", ".checkbox",
+			function(){
+				//modify div properties AND erase one from the count thingy
+				if($(this).prop("checked")){
+					$(this).parent().css("text-decoration", "line-through" );
+				} else {
+					$(this).parent().css("text-decoration", "none" );
+				}
+				check();
+			}
+		);
+		$("main").on("click", ".eraseButton",
+			function(){
+				$(this).parent().remove();
+				check();
+			}
+		);
 
     }
     $(window).load(setup);	
